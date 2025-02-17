98d1c226b575a067ac571beb841905773c01008a
Andrew Ganzha <jackbrikman@mail.ru>
2025-02-17 17:20:29 +0300

    add body writer

--- internal/auth/handler.go
+++ internal/auth/handler.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"encoding/json"
 	"fmt"
 	"learnProject/configs"
 	"learnProject/pkg/response"
@@ -30,6 +31,11 @@ func (handler *AuthHandler) Register() http.HandlerFunc {
 
 func (handler *AuthHandler) Login() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		var payload LoginRequest
+		err := json.NewDecoder(r.Body).Decode(&payload)
+		if err != nil {
+			response.Json(err.Error(), w, http.StatusBadRequest)
+		}
 		res := LoginResponse{
 			Token: "123",
 		}
